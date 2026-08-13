2b32e8634090313c9baa1d6cd1daec46d2a663e1
Deborah Bombini <deborah.bombini@edata.info>
2026-08-13 16:02:02 -0300

    fix for deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", ".prisma/client"],
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
